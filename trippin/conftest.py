226e9587526f1c5d6599bf685a6e381e29acb12c
iiviie <divyanshverma158@gmail.com>
2024-12-29 14:26:42 +0530

    start cookiecutter django project

---
+++ trippin/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from trippin.users.models import User
+from trippin.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
